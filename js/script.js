fec8c3185919ec195735470dfb30f0343154a365
augusto <augustotognetti@hotmail.com>
2022-10-30 15:57:56 -0300

    front-end completo

--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementsByName("opcao").onclick = function() {mudar_placeholder()};
-
-function mudar_placeholder(){
-
-    valor_radio = document.querySelector("input[name=opcao]:checked").value;
-    document.getElementById("mostrar_resultado").innerHTML = valor_radio;
-
-}
